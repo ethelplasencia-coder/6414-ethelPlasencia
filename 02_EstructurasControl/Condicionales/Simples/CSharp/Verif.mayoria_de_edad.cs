79a83b8f56dc365a80ce822b3adeaf4f51445778
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-22 01:16:39 -0500

    Se agrego un ejemplo de estructura condicional simple en CSharp

---
+++ 02_EstructurasControl/Condicionales/Simples/CSharp/Verif.mayoria_de_edad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verif.mayoria_de_edad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Verificación de Mayoría de Edad ===");
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese su edad:");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+           
+            if (edad >= 18)
+            {
+                Console.WriteLine("Eres mayor de edad.");
+            }
+
+            Console.WriteLine("Programa finalizado.");
+            Console.ReadKey();
+
+        }
+    }
+}
