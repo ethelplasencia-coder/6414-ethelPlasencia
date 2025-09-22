aa3e84ae52a4f3b53d00b10e7eaccad7246d2b95
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-22 02:05:00 -0500

    Traducciòn del ejemplo 1 de estructura Condicional simple a Python

---
+++ 02_EstructurasControl/Condicionales/Simples/Python/Verif.mayoria_de_edad.py
@@ -0,0 +1,10 @@
+# === Verificación de Mayoría de Edad ===
+
+# Pedir la edad
+edad = int(input("Ingrese su edad: "))
+
+# Condicional simple
+if edad >= 18:
+    print("Eres mayor de edad.")
+
+print("Programa finalizado.")
