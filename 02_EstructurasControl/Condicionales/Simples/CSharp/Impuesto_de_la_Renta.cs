21b8c0a3dc5f6a0e612e8c6451ff121f3ef21dcc
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-22 01:52:35 -0500

    Se agrego un segundo ejemplo a la carpeta de Condicionales simples en CSharp

---
+++ 02_EstructurasControl/Condicionales/Simples/CSharp/Impuesto_de_la_Renta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impuesto_a_la_Renta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Cálculo de Impuesto a la Renta ===");
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese el monto de su renta en soles:");
+            double renta = Convert.ToDouble(Console.ReadLine());
+
+            // Condicional simple
+            if (renta > 4000)
+            {
+                double impuesto = renta * 0.05;
+                Console.WriteLine("El impuesto a pagar es: S/ " + impuesto);
+            }
+
+
+        }
+    }
+}
