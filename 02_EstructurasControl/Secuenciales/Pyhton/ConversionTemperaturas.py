d798ab63e8ceb58e4aea56d1e9f4a03c128843e5
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-22 00:10:21 -0500

    Traducciòn del ejemplo 2 a python

---
+++ 02_EstructurasControl/Secuenciales/Pyhton/ConversionTemperaturas.py
@@ -0,0 +1,13 @@
+print("CONVERSIÓN DE TEMPERATURAS")
+print("==========================")
+
+celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+kelvin = celsius + 273.15
+
+print("==========================")
+print("La temperatura en Fahrenheit es:", fahrenheit)
+print("La temperatura en Kelvin es:", kelvin)
+
+input("Presione Enter para salir...")
