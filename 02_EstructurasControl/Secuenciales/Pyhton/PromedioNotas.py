08c76c9ce2764d872afff3fd0558f1a2522a4765
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-21 23:50:15 -0500

    Traducciòn del ejemplo 1 a Python

---
+++ 02_EstructurasControl/Secuenciales/Pyhton/PromedioNotas.py
@@ -0,0 +1,16 @@
+print("CALCULAR PROMEDIO DE NOTAS")
+print("==========================")
+
+# Pedir las 3 notas
+nota1 = float(input("Nota 1: "))
+nota2 = float(input("Nota 2: "))
+nota3 = float(input("Nota 3: "))
+
+# Calcular el promedio
+promedio = (nota1 + nota2 + nota3) / 3
+
+# Mostrar resultado
+print("==========================")
+print("PROMEDIO:", format(promedio, ".2f"))
+
+
