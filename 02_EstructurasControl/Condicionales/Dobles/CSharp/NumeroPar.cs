251faa19067bec1442c489241ff25dcad1b32e4f
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-22 02:37:55 -0500

    Se agrego un ejemplo de Condicional Doble en CSharp

---
+++ 02_EstructurasControl/Condicionales/Dobles/CSharp/NumeroPar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Verificación de Número Par ===");
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese un número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            // Condicional doble
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El número es par.");
+            }
+            else
+            {
+                Console.WriteLine("El número es impar.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
