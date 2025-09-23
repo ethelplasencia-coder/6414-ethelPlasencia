fc1ddc29dcd2e37127be9f27202d1f371d6da213
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-22 23:23:26 -0500

    Traducciòn del ejemplo 1 de Condicional doble a Python

---
+++ 02_EstructurasControl/Condicionales/Dobles/Python/NumeroPar.py
@@ -0,0 +1,10 @@
+print ("=== Verificación de Número Par ===")
+print ()
+1
+numero=int(input("Ingrese un nùmero: "))
+
+if numero % 2 == 0:
+    print ("El numero es par.")
+    
+else:
+    print ("El numero es impar.")
