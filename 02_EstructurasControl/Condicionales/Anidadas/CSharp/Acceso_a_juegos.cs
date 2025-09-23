186846ae1b72908b9b87bb118880f8c11da97030
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-23 02:10:33 -0500

    Se agrego el segundo ejemplo de condicional anidada en Csharp

---
+++ 02_EstructurasControl/Condicionales/Anidadas/CSharp/Acceso_a_juegos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acceso_a_juegos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Este programa verifica si se puede ingresar a la montaña rusa teniendo en cuenta la edad y la estatura.
+
+            Console.WriteLine("Ingrese su edad:");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su estatura en cm:");
+            int estatura = Convert.ToInt32(Console.ReadLine());
+
+            if (edad >= 12)
+            {
+                Console.WriteLine("Puedes entrar a la montaña rusa.");
+
+                if (estatura < 140)
+                {
+                    Console.WriteLine("Pero debes ir acompañado de un adulto por tu estatura.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Puedes entrar solo a los juegos infantiles.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
