55a802a24941974ca9016136f3b4e2f8c81571fa
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-22 23:05:46 -0500

    Se agrego un segundo ejemplo de Condicional Doble en CSharp

---
+++ 02_EstructurasControl/Condicionales/Dobles/CSharp/Calcular_pagohorasextras.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_pagohorasextras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double horas, horasextras, tarifa, pago;
+
+            Console.WriteLine("Ingrese las horas de trabajo");
+            horas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la tarifa por hora");
+            tarifa = Convert.ToDouble(Console.ReadLine());
+
+            if (horas <= 40)
+            {
+                pago = horas * tarifa;
+            }
+            else
+            {
+                horasextras = horas - 40;
+                pago = (40 * tarifa) + (horasextras * tarifa * 1.15);
+            }
+            Console.WriteLine("El pago es: " + pago);
+
+        }
+    }
+}
