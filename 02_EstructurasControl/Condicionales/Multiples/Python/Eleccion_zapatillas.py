c4abf48f0e112f8894e7e80558bb686f015d1e6f
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-24 12:24:15 -0500

    Traducciòn del ejemplo 1 de Condicional multiple a Python

---
+++ 02_EstructurasControl/Condicionales/Multiples/Python/Eleccion_zapatillas.py
@@ -0,0 +1,23 @@
+marca = str(input("Ingrese la marca que desee (ADIDAS,NIKE,PUMA)"))
+if (marca == "ADIDAS"):
+    print("---ZAPATILLAS ADIDAS---")
+    print()
+    print("Adidas Ultraboost Light")
+    print("Adidas NMD_R1")
+    print("Adidas Samba OG")
+    
+elif marca == "NIKE":
+    print("---ZAPATILLAS NIKE---")
+    print("Nike Air Force 1")
+    print("Nike Air Max 90")
+    print("Nike Dunk Low")
+
+elif marca == "PUMA":
+    print("---ZAPATILLAS PUMA---")
+    print("Puma Suede Classic")
+    print("Puma RS-X")
+    print("Puma Cali")
+
+else:
+    print("Marca no reconocida")
+
