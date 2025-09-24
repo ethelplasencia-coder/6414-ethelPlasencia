14a59ee77808ab8b4adc673f4f0be57da76ee555
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-24 14:19:23 -0500

    Se agrego un ejemplo de Repetitiva for en Csharp

---
+++ 02_EstructurasControl/Repetitivas/For/CSharp/Calcular_ahorroSemanal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calcula el ahorro semanal 
+            double ahorro = 0;
+            Console.WriteLine("¿Cuanto ahorras por dìa?");
+            double dinero = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 1; i <= 7; i++)
+            {
+
+                ahorro = ahorro + dinero;
+
+            }
+            Console.WriteLine("Tu ahorro total durante la semana es de: " + ahorro);
+            Console.ReadKey (); 
+        }
+    }
+}
