332c600eeb9f2c0237310ce32c5bdf6e30d30a0c
EthelPlasencia <ethelplasencia@gmail.com>
2025-09-25 05:28:57 -0500

    Se agrego el ejemplo 1 y 2 de Repetitiva do-while en CSharp

---
+++ 02_EstructurasControl/Repetitivas/Do-While/CSharp/Adivina_BTS.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adivina_BTS.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra, respuesta;
+             
+            Console.WriteLine("Escribe un numero del 1 a 7 o SALIR, y adivina el personaje de BTS");
+            palabra = Console.ReadLine();
+
+            do
+            {
+                Console.Write("\nTu numero es : " + palabra );
+                
+
+                if (palabra == "1")
+                {
+                    Console.WriteLine(" Es el menor del grupo. ¿quien es?");
+                    respuesta = Console.ReadLine();
+                    if (respuesta == "Jungkook")
+                    {
+                        Console.WriteLine("¡ADIVINASTE!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("¡NO ADIVINASTE!");
+                    }
+                }
+                else if (palabra == "2")
+                {
+                    Console.WriteLine(" Es el mayor del grupo. ¿quien es?");
+                    respuesta = Console.ReadLine();
+                    if (respuesta == "Jin")
+                    {
+                       Console.WriteLine("¡ADIVINASTE!");
+                        break;
+                    }  
+                    else
+                    {
+                       Console.WriteLine("¡NO ADIVINASTE!");
+                    }
+                }
+                else if (palabra == "3") 
+                {
+                     Console.WriteLine(" Es el lider del grupo. ¿quien es?");
+                     respuesta = Console.ReadLine();
+                     if (respuesta == "RM")
+                     {
+                         Console.WriteLine("¡ADIVINASTE!");
+                         break;
+                     }  
+                     else
+                     {
+                         Console.WriteLine("¡NO ADIVINASTE!");
+                     }
+                }
+                else if (palabra == "4")
+                {
+                    Console.WriteLine(" Es el màs dormilon del grupo. ¿quien es?");
+                    respuesta = Console.ReadLine();
+                    if (respuesta == "Suga")
+                    {
+                        Console.WriteLine("¡ADIVINASTE!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("¡NO ADIVINASTE!");
+                    }
+                }
+                else if (palabra == "5")
+                {
+                    Console.WriteLine(" Tiene personalidad amistosa, carismática y alegre. ¿quien es?");
+                    respuesta = Console.ReadLine();
+                    if (respuesta == "V")
+                    {
+                        Console.WriteLine("¡ADIVINASTE!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("¡NO ADIVINASTE!");
+                    }
+                }
+                else if (palabra == "6")
+                {
+                    Console.WriteLine(" Es conocido como \"La luz del sol\" ¿quien es?");
+                    respuesta = Console.ReadLine();
+                    if (respuesta == "Jhope")
+                    {
+                        Console.WriteLine("¡ADIVINASTE!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("¡NO ADIVINASTE!");
+                    }
+                }
+                else if (palabra == "7")
+                {
+                    Console.WriteLine(" Es el màs pequeño del grupo. ¿quien es?");
+                    respuesta = Console.ReadLine();
+                    if (respuesta == "Jimin")
+                    {
+                        Console.WriteLine("¡ADIVINASTE!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("¡NO ADIVINASTE!");
+                    }
+                }
+                if (palabra.ToUpper() == "SALIR")
+                {
+                    break; 
+                }
+
+            } while (true);
+            Console.WriteLine(" ¡Puerta abierta!");
+        }
+    }
+}
---
+++ 02_EstructurasControl/Repetitivas/Do-While/CSharp/Adivina_palabrasecreta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adivina_palabrasecreta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+
+            do
+            {
+                Console.WriteLine("===PISTAS===");
+                Console.WriteLine("Es una estrella de plasma");
+                Console.WriteLine("Es muy caliente");
+                Console.WriteLine();
+                Console.Write("Escribe la palabra secreta: ");
+                palabra = Console.ReadLine();
+            } while (palabra != "sol");
+
+            Console.WriteLine("¡Correcto!");
+        }
+    }
+}
